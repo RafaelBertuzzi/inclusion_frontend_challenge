aa8cac5bdf0b9ac2e72fe5c68eabffdfa64d45a0
Rafael Bertuzzi <rafael.bertuzzi@gmail.com>
2024-01-12 16:53:30 -0300

    test: fix test run

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,21 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.paths.json');
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  jest: {
+    configure: {
+      preset: 'ts-jest',
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: '<rootDir>/',
+      }),
     },
   },
 };
