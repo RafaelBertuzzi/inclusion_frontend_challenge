641d2614ede11453d70ed9857f52bf86659ed76b
Rafael Bertuzzi <rafael.bertuzzi@gmail.com>
2024-01-12 16:17:56 -0300

    feat: add ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <ThemeProvider theme={theme}>
+      <header>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    danger: '#F94D03',
+    success: '#0BAB38',
+    alert: '#F9C303',
+    info: '#73F5FD',
+    dark: '#000000',
+    gray: '#868686',
+  },
+};
