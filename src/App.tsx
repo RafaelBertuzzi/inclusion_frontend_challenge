909e1cd5c1b30b0081a933fb3beea1ac6e15fa02
Rafael Bertuzzi <rafael.bertuzzi@gmail.com>
2024-01-12 16:22:44 -0300

    feat: add store and reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <header>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
